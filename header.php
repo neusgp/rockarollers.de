91f3b826f498ffa1184eb0cb56643df098907e68
Neus Garcia Puigdollers <neuspiano@gmail.com>
2025-05-02 13:41:25 +0200

    style: header

--- header.php
+++ header.php
@@ -9,19 +9,19 @@
 
 <body <?php body_class() ?>>
     <header id='header'>
-        <div></div>
-
+        <img class='logo' src='http://rockarollers.local/wp-content/uploads/2014/12/rockarollers_logo.png' high='160' width='160' alt='Rockarollers Logo' aria-label='Rockarollers Logo' />
         <?php
         wp_nav_menu(array(
             'depth' => 1,
-            'theme_location' => 'header'
+            'theme_location' => 'social',
+            'menu_class' => 'social social-header menu'
         ));
         ?>
         <?php
         wp_nav_menu(array(
             'depth' => 1,
-            'theme_location' => 'social'
+            'theme_location' => 'header',
+            'menu_class' => 'main-menu-header menu'
         ));
         ?>
-
     </header>
