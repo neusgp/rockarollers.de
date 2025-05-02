76bc51e27bb814bd42af69fff32922ed08b1feff
Neus Garcia Puigdollers <neuspiano@gmail.com>
2025-05-02 13:41:08 +0200

    style: footer

--- footer.php
+++ footer.php
@@ -1,6 +1,7 @@
 <footer>
-    <div>
-        <div>
+    <div class='menu-footer'>
+        <img class='wftda-logo' src='https://rockarollers.de/wp-content/uploads/2015/11/WFTDA_logo.png' width='132' alt='WFTDA Logo' aria-label='Rockarollers Logo' />
+        <div class='menu-section-footer'>
             <h3>Kontakt</h3>
             <?php
             $kontakt_page_content = get_page_by_path('kontakt');
@@ -9,40 +10,45 @@
             }
             ?>
         </div>
-        <div>
+        <div class='menu-section-footer'>
             <h3>Interesting Links</h3>
             <?php
             wp_nav_menu(array(
                 'depth' => 1,
-                'theme_location' => 'links'
+                'theme_location' => 'links',
+                'menu_class' => 'main-menu-footer menu'
             ));
             ?>
         </div>
-        <div>
+        <div class='menu-section-footer'>
             <h3>Presse</h3>
             <?php
             wp_nav_menu(array(
                 'depth' => 1,
-                'theme_location' => 'presse'
+                'theme_location' => 'presse',
+                'menu_class' => 'main-menu-footer menu'
             ));
             ?>
         </div>
-        <div>
+        <div class='menu-section-footer'>
             <h3>Social</h3>
             <?php
             wp_nav_menu(array(
                 'depth' => 1,
-                'theme_location' => 'social'
+                'theme_location' => 'social',
+                'menu_class' => 'social menu'
             ));
             ?>
         </div>
     </div>
-    <div>
+    <div class='credits-footer'>
         <p>© 2025 Neus Garcia für Rockarollers</p>
         <?php
         wp_nav_menu(array(
             'depth' => 1,
-            'theme_location' => 'footer'
+            'theme_location' => 'footer',
+            'menu_class' => 'credits-menu-footer menu'
+
         ));
         ?>
     </div>
