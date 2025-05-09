7d618d1375264515e79006da7d7477443f06f224
Neus Garcia Puigdollers <neuspiano@gmail.com>
2025-05-09 14:52:40 +0200

    feat: render sponsors

--- front-page.php
+++ front-page.php
@@ -1,7 +1,9 @@
 <?php get_header(); ?>
 
-<?php while (have_posts()) : the_post(); ?>
-    <div id='content'>
+
+<div id='content'>
+    <?php while (have_posts()) : the_post(); ?>
+        <!-- HERO -->
         <div id='hero-section'>
             <img class='background-image' src='<?php the_field('hero_background_image'); ?>' />
             <h1><?php the_field('hero_section_title'); ?></h1>
@@ -11,10 +13,12 @@
                 <a href='<?php the_field('link_hero_button_2'); ?>' class='primary-button-yellow'><?php the_field('label_hero_button_2'); ?></a>
             </div>
         </div>
+        <!-- NEWS -->
         <div id='news-section' class='section'>
             <h2><?php the_field('news_section_title'); ?></h2>
             <?php echo do_shortcode('[instagram-feed feed=1]'); ?>
         </div>
+        <!-- TEAM -->
         <div id='team-section' class='section'>
             <div>
                 <h2><?php the_field('team_section_title'); ?></h2>
@@ -23,53 +27,78 @@
             </div>
             <div id='horizontal-scroll'></div>
         </div>
+        <!-- EVENTS -->
         <div id='events-section' class='section'>
             <img class='background-image' src='<?php the_field('events_background_image'); ?>' />
             <h2><?php the_field('events_section_title'); ?> <span class='yellow-highlight'><?php the_field('events_section_year'); ?></span></h2>
+        <?php endwhile; ?>
+        <div class='events-preview-list'>
             <?php
-            $homePageEventList = new WP_Query(array('post_type' => 'event'));
+            $homePageEventList = new WP_Query(array('post_type' => 'event', 'posts_per_page' => -1));
             if (!$homePageEventList->have_posts()) {
             ?> <p>No events found</p>
             <?php
             } ?>
-            <div class='events-preview-list'>
-                <?php
-                while ($homePageEventList->have_posts()) {
-                    $homePageEventList->the_post(); ?>
-                    <div class='event-preview'>
-                        <div class='event-preview-date'>
-                            <?php
-                            //separate month and day
-                            $date = get_field('date');
-                            $parsedDate = explode(',', $date);
-                            $stringParsedDate = implode(' ', $parsedDate);
-                            $finalParsedData = explode(' ', $stringParsedDate);
-                            ?>
-                            <div class='event-month'>
-                                <p><?php echo $finalParsedData[0] ?></p>
-                            </div>
-                            <div class='event-day'>
-                                <p><?php echo $finalParsedData[1] ?></p>
-                            </div>
+            <?php
+            while ($homePageEventList->have_posts()) {
+                $homePageEventList->the_post(); ?>
+                <div class='event-preview'>
+                    <div class='event-preview-date'>
+                        <?php
+                        //separate month and day
+                        $date = get_field('date');
+                        $parsedDate = explode(',', $date);
+                        $stringParsedDate = implode(' ', $parsedDate);
+                        $finalParsedData = explode(' ', $stringParsedDate);
+                        ?>
+                        <div class='event-month'>
+                            <p><?php echo $finalParsedData[0] ?></p>
                         </div>
-                        <div class='event-preview-info'>
-                            <p class='event-preview-title'><?php the_field('title') ?></p>
-                            <div class='event-preview-place-time'>
-                                <p><i class='fa fa-map-marker'></i> <?php the_field('place') ?></p>
-                                <p><i class='fa fa-clock-o'></i> <?php the_field('time') ?></p>
-                            </div>
+                        <div class='event-day'>
+                            <p><?php echo $finalParsedData[1] ?></p>
                         </div>
                     </div>
+
+                    <div class='event-preview-info'>
+                        <p class='event-preview-title'><?php the_field('title') ?></p>
+                        <div class='event-preview-place-time'>
+                            <p><i class='fa fa-map-marker'></i> <?php the_field('place') ?></p>
+                            <p><i class='fa fa-clock-o'></i> <?php the_field('time') ?></p>
+                        </div>
+                    </div>
+                </div>
+            <?php
+            }
+            ?>
+        </div>
+        </div>
+
+        <?php while (have_posts()) : the_post(); ?>
+            <!-- SPONSORS -->
+            <div id='sponsors-section' class='section'>
+                <h2><?php the_field('sponsors_section_title'); ?></h2>
+            <?php endwhile; ?>
+            <div class='sponsors-list'>
+                <?php
+                $homePageSponsorsList = new WP_Query(array('post_type' => 'sponsor', 'posts_per_page' => -1));
+                if (!$homePageSponsorsList->have_posts()) {
+                ?> <p>No sponsors found</p>
+                <?php
+                } ?>
+                <?php
+                while ($homePageSponsorsList->have_posts()) {
+                    $homePageSponsorsList->the_post(); ?>
+                    <img class='sponsor-logo' src='<?php the_field('logo'); ?>' />
                 <?php
                 }
                 ?>
-            </div>
-        </div>
-        <div id='sponsors-section' class='section'>
-            <h2><?php the_field('sponsors_section_title'); ?></h2>
-        </div>
-    </div>
-<?php endwhile; ?>
 
+            </div>
+            <?php while (have_posts()) : the_post(); ?>
+                <a href='<?php the_field('link_sponsors_button'); ?>' class='sponsors-section-button primary-button-yellow'><?php the_field('label_sponsors_button'); ?></a>
+            </div>
+        <?php endwhile; ?>
+
+</div>
 
 <?php get_footer(); ?>
