31687b5d7e2b0d2046354ef4e144651ea6ddcb98
Neus Garcia Puigdollers <neuspiano@gmail.com>
2025-05-13 16:48:13 +0200

    feat: trainingszeiten

--- page-mitmachen-2.php
+++ page-mitmachen-2.php
@@ -6,11 +6,11 @@ get_header();
 <div id='content'>
     <?php while (have_posts()) : the_post(); ?>
         <section id='banner'>
-
             <img class='background-image' src='<?php the_field('mitmachen_banner_image'); ?>' />
             <h2><?php the_title(); ?></h2>
         </section>
-        <section id='newbiekurs'>
+        <!-- INFOS -->
+        <section id='mitmachen-infos'>
             <div>
                 <div>
                     <h2><?php the_field('mitmachen_newbiekurs_title'); ?></h2>
@@ -24,29 +24,41 @@ get_header();
                 <p><?php the_field('mitmachen_trainings_text'); ?></p>
                 <a href='<?php the_field('mitmachen_trainings_button_link'); ?>' class='link-button'><?php the_field('mitmachen_trainings_button_label'); ?></a>
             </div>
-
         </section>
-    <?php endwhile; ?>
-    <section id='trainings'>
+        <!-- TRAININGSZEITEN -->
+        <section id='trainingszeiten'>
+            <img class='background-image' src='<?php the_field('trainingszeiten_banner_image'); ?>' />
+        <?php endwhile; ?>
         <?php
-        $trainingEventsList = new WP_Query(array('post_type' => 'event'));
-        while ($trainingEventsList->have_posts()) {
-            if (!$trainingEventsList->have_posts()) { ?>
-                <p>No trainings found</p>
+        $trainingEventsList = new WP_Query(array('post_type' => 'event')); ?>
+        <div class='trainings-preview-list'>
             <?php
-            }
-            $trainingEventsList->the_post();
-            if (get_field('type') === 'training') {
-            ?>
-                <p><?php the_field('title'); ?></p>
-                <p><?php the_field('start_time'); ?> - <?php the_field('end_time'); ?></p>
-                <p><?php the_field('place'); ?></p>
-        <?php
+            while ($trainingEventsList->have_posts()) {
+                if (!$trainingEventsList->have_posts()) { ?>
+                    <p>No trainings found</p>
+                <?php
+                }
+                $trainingEventsList->the_post();
+                if (get_field('type') === 'training') {
+                ?><div class='trainingzeit-preview'>
+                        <div class='training-title'>
+                            <span><?php the_field('title'); ?></span>
+                        </div>
+                        <div class='training-infos'>
+                            <p><?php the_field('description'); ?></p>
+                            <div>
+                                <p><i class='fa fa-clock-o'></i> <?php the_field('start_time'); ?> - <?php the_field('end_time'); ?></p>
+                                <p><i class='fa fa-map-marker'></i> <?php the_field('place'); ?></p>
+                            </div>
+                        </div>
+                    </div>
+            <?php
 
+                }
             }
-        }
-        ?>
-    </section>
+            ?>
+        </div>
+        </section>
 </div>
 <?php
 get_footer();
