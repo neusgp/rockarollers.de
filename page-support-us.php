7f3701fef567dbd4a01bf38864382f033d5124db
Neus Garcia Puigdollers <neuspiano@gmail.com>
2025-05-19 10:06:27 +0200

    feat: support-us page

---
+++ page-support-us.php
@@ -0,0 +1,41 @@
+<?php
+
+get_header();
+
+?>
+<div id='content'>
+    <?php while (have_posts()) : the_post(); ?>
+        <section id='banner'>
+            <img class='background-image' src='<?php the_field('support_banner_image'); ?>' />
+            <h2><?php the_title(); ?></h2>
+        </section>
+        <section id='sponsors-text'>
+            <p><?php the_field('support_text'); ?></p>
+            <p class='email'></strong><?php the_field('support_email'); ?></p>
+        </section>
+    <?php endwhile; ?>
+    <section id='sponsors-list'>
+        <h2><?php the_field('support_sponsors_list_title'); ?></h2>
+        <div class='sponsors-list'>
+            <?php
+            $homePageSponsorsList = new WP_Query(array('post_type' => 'sponsor', 'posts_per_page' => -1));
+            if (!$homePageSponsorsList->have_posts()) {
+            ?> <p>No sponsors found</p>
+            <?php
+            } ?>
+            <?php
+            while ($homePageSponsorsList->have_posts()) {
+                $homePageSponsorsList->the_post(); ?>
+                <img class='sponsor-logo' src='<?php the_field('logo'); ?>' />
+            <?php
+            }
+            ?>
+        </div>
+    </section>
+
+</div>
+<?php
+
+get_footer();
+
+?>
