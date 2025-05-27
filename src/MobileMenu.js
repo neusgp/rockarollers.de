c11f24cd15e1054b9553b24a6cce78844708b7ca
Neus Garcia Puigdollers <neuspiano@gmail.com>
2025-05-27 14:15:24 +0200

    feat: close mobile menu when link is clicked

--- src/MobileMenu.js
+++ src/MobileMenu.js
@@ -3,6 +3,8 @@ const hamburgerIcon = document.querySelector(".hamburger-menu-icon");
 hamburgerIcon.addEventListener("click", () => {
   let mobileMenu;
   let closeButton;
+  let links;
+
   console.log("open mobile menu");
   mobileMenu = document.querySelector(".mobile-menu");
 
@@ -11,8 +13,16 @@ hamburgerIcon.addEventListener("click", () => {
 
     closeButton = document.querySelector(".close-button");
     if (closeButton)
-      closeButton.addEventListener("click", () => {
-        mobileMenu.classList.remove("show");
-      });
+      closeButton.addEventListener("click", () =>
+        mobileMenu.classList.remove("show")
+      );
+
+    links = document.getElementsByTagName("a");
+    if (links)
+      for (let link of links) {
+        link.addEventListener("click", () =>
+          mobileMenu.classList.remove("show")
+        );
+      }
   }
 });
