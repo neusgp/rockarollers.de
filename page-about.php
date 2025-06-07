61c339c5d8f0ef336b9ce8df64594fa7c309135e
Neus Garcia Puigdollers <neuspiano@gmail.com>
2025-06-07 11:34:36 +0200

    styles: hover and other details

--- page-about.php
+++ page-about.php
@@ -14,9 +14,9 @@ get_header();
             <div class='about-roller-derby-content flex-row gap'>
                 <div>
                     <p class='paragraph'><?php the_field('about_roller_derby_text'); ?></p>
-                    <div class='about-interesting-links flex-row gap link-hover'>
-                        <a href=<?php the_field('interesting_link_1'); ?>> <img src='<?php the_field('interesting_link_1_image'); ?>' /></a>
-                        <a href=<?php the_field('interesting_link_2'); ?>> <img src='<?php the_field('interesting_link_2_image'); ?>' /></a>
+                    <div class='about-interesting-links flex-row gap'>
+                        <a class='link-hover' href=<?php the_field('interesting_link_1'); ?>> <img src='<?php the_field('interesting_link_1_image'); ?>' /></a>
+                        <a class='link-hover' href=<?php the_field('interesting_link_2'); ?>> <img src='<?php the_field('interesting_link_2_image'); ?>' /></a>
                     </div>
                 </div>
                 <img src='<?php the_field('about_roller_derby_image'); ?>' />
