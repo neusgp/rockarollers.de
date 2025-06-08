c6758bc38174c0ebd1d3037fdbfea9139bf4871b
Neus Garcia Puigdollers <neuspiano@gmail.com>
2025-06-08 15:40:28 +0200

    styles: fix team page styles

--- page-teams.php
+++ page-teams.php
@@ -12,19 +12,19 @@ get_header();
         <section id='team-links'>
             <div class='team-links'>
                 <a class='team-link' href='<?php the_field('rockarollers_link'); ?>'>
-                    <img class='background-image' src='<?php the_field('rockarollers_link_image'); ?>' />
+                    <img src='<?php the_field('rockarollers_link_image'); ?>' />
                     <p><?php the_field('rockarollers_link_title'); ?></p>
                 </a>
                 <a class='team-link' href='<?php the_field('rockabillies_link'); ?>'>
-                    <img class='background-image' src='<?php the_field('rockabillies_link_image'); ?>' />
+                    <img src='<?php the_field('rockabillies_link_image'); ?>' />
                     <p><?php the_field('rockabillies_link_title'); ?></p>
                 </a>
                 <a class='team-link' href='<?php the_field('trainers_link'); ?>'>
-                    <img class='background-image' src='<?php the_field('trainers_link_image'); ?>' />
+                    <img src='<?php the_field('trainers_link_image'); ?>' />
                     <p><?php the_field('trainers_link_title'); ?></p>
                 </a>
                 <a class='team-link' href='<?php the_field('officials_link'); ?>'>
-                    <img class='background-image' src='<?php the_field('officials_link_image'); ?>' />
+                    <img src='<?php the_field('officials_link_image'); ?>' />
                     <p><?php the_field('officials_link_title'); ?></p>
                 </a>
             </div>
