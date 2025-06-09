86af3b65a6b1146260352d7c5c10e09d4d2d317e
Neus Garcia Puigdollers <neuspiano@gmail.com>
2025-06-09 13:11:43 +0200

    fix: mobile menu

--- src/MobileMenu.js
+++ src/MobileMenu.js
@@ -1,4 +1,8 @@
 const hamburgerIcon = document.querySelector(".hamburger-menu-icon");
+const content = document.querySelector("#content");
+const footer = document.querySelector("footer");
+
+console.log(content, footer);
 
 hamburgerIcon.addEventListener("click", () => {
   let mobileMenu;
@@ -9,19 +13,25 @@ hamburgerIcon.addEventListener("click", () => {
 
   if (mobileMenu) {
     mobileMenu.classList.add("show");
+    content.classList.add("hide");
+    footer.classList.add("hide");
 
     closeButton = document.querySelector(".close-button");
     if (closeButton)
-      closeButton.addEventListener("click", () =>
-        mobileMenu.classList.remove("show")
-      );
+      closeButton.addEventListener("click", () => {
+        mobileMenu.classList.remove("show");
+        content.classList.remove("hide");
+        footer.classList.remove("hide");
+      });
 
     links = document.getElementsByTagName("a");
     if (links)
       for (let link of links) {
-        link.addEventListener("click", () =>
-          mobileMenu.classList.remove("show")
-        );
+        link.addEventListener("click", () => {
+          mobileMenu.classList.remove("show");
+          content.classList.remove("hide");
+          footer.classList.remove("hide");
+        });
       }
   }
 });
